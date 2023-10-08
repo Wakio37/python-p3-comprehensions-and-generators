ba5bad98b6f9e4f715c6be695fa3e4d49ace49b9
Wakio37 <your_email@example.com>
2023-10-08 13:23:27 +0000

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [x for x in num_list if x % 2 == 0]
+    return even_numbers
+
+result = return_evens([0, 1, 3, 5, 7, 8, 9])
+print(result)  
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentences = [sentence + "!" for sentence in sentence_list]
+    return exclamation_sentences
+
+result = make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print(result)  
